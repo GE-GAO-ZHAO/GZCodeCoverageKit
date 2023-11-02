049376f28a162091f92a2d932ef57a307bf35b19
葛高召 <gaozhao.ge@huolala.cn>
2023-11-02 14:27:20 +0800

    Initial commit

---
+++ Example/GZCodeCoverageKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GZCodeCoverageKit
+//
+//  Created by 葛高召 on 11/02/2023.
+//  Copyright (c) 2023 葛高召. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
